0418398487b20ca08cf1165efaa89862d22cdb71
BILL <106547083+zggfds@users.noreply.github.com>
2024-09-15 23:24:51 +0500

    yey

---
+++ cheat.lua
@@ -0,0 +1,3 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+local Window = Library.CreateLib("BILL", "RJTheme3")
+local Tab = Window:NewTab("Slap Battles")
