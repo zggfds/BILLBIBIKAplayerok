49e39b7612685835b190a6b55a1394b03113d213
BILL <106547083+zggfds@users.noreply.github.com>
2024-09-15 22:01:55 +0300

    Update cheat.lua

--- cheat.lua
+++ cheat.lua
@@ -7,3 +7,12 @@ local Window = Library.CreateLib("playerokBILL Hub | Slap Battles", "DarkTheme")
 local Tab = Window:NewTab("PLAYEROK")
 local Section = Tab:NewSection("Creator")
 local Section2 = Tab:NewSection("Script")
+Section:NewButton("Click to copy", "If You Have Bug Or Ideas DM Me", function()
+    setclipboard('kykyryzo8')
+end)
+    
+local Section = Tab:NewSection("Creator UI and Helper with function")
+    
+Section:NewButton("Giangplay", "INFO", function()
+    game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Creator UI",Text = "Giangplay",Icon = "rbxassetid://7733955511",Duration = 5})
+end)
