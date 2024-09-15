6a9f7e701e8c445fab25e032e3420197911bb233
BILL <106547083+zggfds@users.noreply.github.com>
2024-09-15 21:51:07 +0300

    Update cheat.lua

--- cheat.lua
+++ cheat.lua
@@ -1,4 +1,4 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 local Window = Library.CreateLib("BILL", "RJTheme3")
 local Tab = Window:NewTab("Slap Battles")
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Welcome!",Text = "Welcome to Hub Kykyryz0B.",Icon = "rbxassetid://7733960981",Duration = 10})
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Welcome!",Text = "Welcome to Hub BILL.",Icon = "rbxassetid://7733960981",Duration = 10})
