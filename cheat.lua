f330b2991819f82c95c8518c7b1168dbdf16d60e
BILL <106547083+zggfds@users.noreply.github.com>
2024-09-15 22:09:37 +0300

    Update cheat.lua

--- cheat.lua
+++ cheat.lua
@@ -1,6 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("BILL", "RJTheme3")
-local Tab = Window:NewTab("Slap Battles")
+
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Welcome!",Text = "Welcome to Hub BILL.",Icon = "rbxassetid://7733960981",Duration = 10})
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Kavo.lua"))()
 local Window = Library.CreateLib("playerokBILL Hub | Slap Battles", "DarkTheme")
@@ -11,7 +9,13 @@ Section:NewButton("Click to copy", "If You Have Bug Or Ideas DM Me", function()
     setclipboard('kykyryzo8')
 end)
     
-local Section = Tab:NewSection("Creator UI and Helper with function")
+Section:NewButton("Start script HUB TRAP", "YEY", function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/UPDATE-Slap-Battles-hub-that-exist-12403"))()
+end)
+
+Section:NewButton("Start script Avatar", "YEY", function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Slap-Battles-KykyryzoB-Hub-SB-9008"))()
+end)
     
 Section:NewButton("Giangplay", "INFO", function()
     game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Creator UI",Text = "Giangplay",Icon = "rbxassetid://7733955511",Duration = 5})
